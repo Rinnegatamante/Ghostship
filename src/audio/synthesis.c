581a8958d5bd17f0365b1e659cb2310e65a6c87f
KiritoDv <kiritodev01@gmail.com>
2026-02-15 16:13:09 -0600

    Added some extra validations just in case, but i don't like them

--- src/audio/synthesis.c
+++ src/audio/synthesis.c
@@ -1150,6 +1150,9 @@ u64 *final_resample(u64 *cmd, struct NoteSynthesisState *synthesisState, s32 cou
 }
 #else
 u64 *final_resample(u64 *cmd, struct Note *note, s32 count, u16 pitch, u16 dmemIn, u32 flags) {
+    if (note->synthesisBuffers == NULL) {
+        return cmd;
+    }
     aSetBuffer(cmd++, /*flags*/ 0, dmemIn, /*dmemout*/ DMEM_ADDR_TEMP, count);
     aResample(cmd++, flags, pitch, VIRTUAL_TO_PHYSICAL2(note->synthesisBuffers->finalResampleState));
     return cmd;
@@ -1303,7 +1306,7 @@ u64 *process_envelope(u64 *cmd, struct NoteSubEu *note, struct NoteSynthesisStat
             aMix(cmd++, 0, /*gain*/ 0x8000, /*in*/ DMEM_ADDR_STEREO_STRONG_TEMP_WET,
                  /*out*/ DMEM_ADDR_WET_RIGHT_CH);
         }
-    } else {
+    } else if (note->synthesisBuffers != NULL) {
 #ifdef VERSION_EU
         aEnvMixer(cmd++, mixerFlags, VIRTUAL_TO_PHYSICAL2(synthesisState->synthesisBuffers->mixEnvelopeState));
 #else
